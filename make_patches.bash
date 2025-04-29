09b1f1e88b3eb2a529992365d4f694bb57e76167
Gitrogatog <rpokexy@gmail.com>
2025-04-29 02:17:43 -0700

    updated patches to include jupyter notebook files

--- make_patches.bash
+++ make_patches.bash
@@ -2,7 +2,7 @@
 
 mkdir -p patches/smi patches/gnn patches/mdm patches/data
 
-for file in `find ./src -name '*.py'`
+for file in `find ./src \( -name '*.py' -o -name '*.ipynb' \) -not -path "*/.ipynb_checkpoints/*"`
 do
 	outfile=`echo $file | sed "s/src/pnnlsolpaper/g"`
 	infile=$file
